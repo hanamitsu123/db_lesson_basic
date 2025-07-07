9ea75a194170b223147f098dfc129baab54d9ee2
hanamitsu123 <tokumitsu777@gmail.com>
2025-07-07 17:50:36 +0900

    クエリ以外の箇所をコメントアウトしました。

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,4 +1,4 @@
-Q1.新たに部署のテーブルを追加してください。
+-- Q1.新たに部署のテーブルを追加してください。
 
   CREATE TABLE departments (
   department_id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -7,15 +7,15 @@ Q1.新たに部署のテーブルを追加してください。
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
   );
 
-Q2.peopleテーブルに新しいカラムを追加して、誰がどこ所属しているかがわかるようにしましょう。
+-- Q2.peopleテーブルに新しいカラムを追加して、誰がどこ所属しているかがわかるようにしましょう。
 
   ALTER TABLE people 
   ADD COLUMN department_id INT unsigned 
   AFTER email;
 
-Q3.departments、people、reportsテーブルにある条件を満たすようなレコードを挿入してください。
+-- Q3.departments、people、reportsテーブルにある条件を満たすようなレコードを挿入してください。
 
- 【department】
+--  【department】
 
   INSERT INTO departments 
   (name) 
@@ -26,7 +26,7 @@ Q3.departments、people、reportsテーブルにある条件を満たすよう�
   ('人事'), 
   ('情報システム');
 
-  【people】
+  -- 【people】
 
   INSERT INTO people 
   (name, email, department_id, age, gender) 
@@ -42,9 +42,10 @@ Q3.departments、people、reportsテーブルにある条件を満たすよう�
   ('小林 大輔', 'daisuke.kobayashi@example.com', 4, 36, 1), 
   ('加藤 さくら', 'sakura.kato@example.com', 5, 31, 2);
 
-  【reports】
+  -- 【reports】
 
-  INSERT INTO reports(person_id, content)VALUES 
+  INSERT INTO reports(person_id, content)
+  VALUES 
   (1, '会議資料をたくさん作成しました。 '), 
   (2, '午前より顧客対応を実施し、午後には契約できました。'), 
   (3, '午前は会議に参加しました。お昼ご飯はオムライスを食べました。'), 
@@ -56,45 +57,46 @@ Q3.departments、people、reportsテーブルにある条件を満たすよう�
   (9, 'コードレビューを行いました。反応は上々でした。'), 
   (10, '月報の提出を完了しました。来月も期間内に提出します。');
 
-  Q4.department_idのNULLを、条件に当てはまる値で埋めるためのクエリを作成してください。
+  -- Q4.department_idのNULLを、条件に当てはまる値で埋めるためのクエリを作成してください。
 
   UPDATE people SET department_id = 2 
   WHERE department_id IS NULL;
 
-Q5.年齢の降順で男性の名前と年齢を取得してください。
+-- Q5.年齢の降順で男性の名前と年齢を取得してください。
 
   SELECT name, age FROM people 
   WHERE gender = 1 ORDER BY age DESC;
 
-Q6.テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明してください。
+-- Q6.テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明してください。
 
   このSQLクエリは、peopleテーブルの中から、department_idカラムの値が1のレコードを対象に、name, email, ageカラムの値を取得し、created_atカラムの値を基準に古い順に並べるものです。
 
-Q7.20代の女性と40代の男性の名前一覧を取得してください。
+-- Q7.20代の女性と40代の男性の名前一覧を取得してください。
 
   SELECT name FROM people 
   WHERE ((gender = 2 AND age BETWEEN 20 and 29) 
   OR (gender = 1 AND age BETWEEN 40 and 49));
 
-Q8.営業部に所属する人だけを年齢の昇順で取得してください。
+-- Q8.営業部に所属する人だけを年齢の昇順で取得してください。
 
   SELECT age FROM people 
   WHERE department_id = 1 ORDER BY age ASC;
 
-Q9.開発部に所属している女性の平均年齢を取得してください。
+-- Q9.開発部に所属している女性の平均年齢を取得してください。
 
   SELECT AVG(age) AS average_age 
   FROM people 
   WHERE gender = 2 AND department_id = 2;
 
-Q10.名前と部署名とその人が提出した日報の内容を同時に取得してください。（日報を提出していない人は含めない）
+-- Q10.名前と部署名とその人が提出した日報の内容を同時に取得してください。（日報を提出していない人は含めない）
 
   SELECT p.name AS person_name, d.name AS department_name, r.content 
   FROM people AS p 
   INNER JOIN departments AS d ON p.department_id = d.department_id 
   INNER JOIN reports AS r ON p.person_id = r.person_id;
 
-Q11.日報を一つも提出していない人の名前一覧を取得してください。
+-- Q11.日報を一つも提出していない人の名前一覧を取得してください。
+
   SELECT p.name 
   FROM people AS p 
   LEFT JOIN reports AS r ON p.person_id = r.person_id 
